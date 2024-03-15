ea0bc5a8102505edb0cda4e1740bcfe600f0f4c5
buenogi <gislayne.bueno@gmail.com>
2024-03-15 10:23:14 -0300

    Extraçao manual dos grupos de qualidade

--- Scripts/02_fechas.R
+++ Scripts/02_fechas.R
@@ -95,8 +95,8 @@ tb1 |>
 
 # Remoção experimentos com fungicida -------------------
 
-read_and_prepare2 <- function(path) {
-  tb <- readxl::read_xlsx(fls[1], sheet = "Fechas") |>
+read_and_prepare <- function(path) {
+  tb <- readxl::read_xlsx(path, sheet = "Fechas") |>
     suppressMessages() |>
     select(11:17)
   nms <- tb[[1]] |>
@@ -106,8 +106,9 @@ read_and_prepare2 <- function(path) {
     cumsum()
   tb <- tb |>
     filter(split >= 1)
+  
   tb <- tb |>
-    filter(!is.na(tb[[2]]), !tb[[2]] %in% c("", "0")) |>
+    filter(!is.na(tb[[1]]), !tb[[1]] %in% c("", "0")) |>
     group_split(split) |>
     map(function(tbi) {
       tbi[, c(1:8)] |>
@@ -116,19 +117,19 @@ read_and_prepare2 <- function(path) {
         suppressMessages() |>
         janitor::clean_names()
     })
+  
   names(tb) <- nms
+  
   tb <- tb |>
     discard(~nrow(.) == 0L) |>
     bind_rows(.id = "experimento") |>
     add_column(
-      filename = tools::file_path_sans_ext(fls[1]),
-      .before = 1
-    ) |>
-    janitor::remove_empty(which = "cols")
-  tb <- openxlsx::convertToDate(tb[[4]])
-  tb <- openxlsx::convertToDate(tb[[5]])
-  tb <- openxlsx::convertToDate(tb[[6]])
-  tb <- openxlsx::convertToDate(tb[[7]])
+      filename = tools::file_path_sans_ext(path),
+      .before = 1) 
+  #|>janitor::remove_empty(which = "cols")
+  tb[[4]] <- openxlsx::convertToDate(tb[[4]])
+  tb[[5]] <- openxlsx::convertToDate(tb[[5]])
+  tb[[6]] <- openxlsx::convertToDate(tb[[6]])
   return(tb)
 }
 
@@ -139,9 +140,33 @@ fix_text <- function(x) {
     str_replace_all(pattern = " +", replacement = " ")
 }
 
-tb <- map(fls, read_and_prepare) |>
-  bind_rows()
-str(tb)
+
+resultados <- list()
+
+
+posicoes_erro <- c()
+
+for (i in seq_along(fls)) {
+  
+  tryCatch({
+    tb <- read_and_prepare(fls[[i]])
+    
+    resultados[[i]] <- tb
+  }, error = function(e) {
+    
+    mensagem_de_erro <- conditionMessage(e)
+    print(paste("Ocorreu um erro na posição", i, ":", mensagem_de_erro))
+    posicoes_erro <- c(posicoes_erro, i)
+  })
+}
+
+
+#resultados <- resultados[-posicoes_erro]
+resultados[[21]] <- NULL
+resultados[[27]] <- NULL
+resultados_sem_x <- map(resultados, ~ select(.x, -starts_with("x")))
+
+tb <- bind_rows(resultados_sem_x)
 
 tb2 <- tb |>
   mutate(cultivar = fix_text(cultivar))
@@ -152,6 +177,8 @@ tb2 |>
 tb2 |>
   count(filename, experimento)
 
+tb1 <- tb1[!grepl("CICLO", tb1$cultivar, ignore.case = TRUE), ]
+tb2 <- tb2[!grepl("CICLO", tb2$cultivar, ignore.case = TRUE), ]
 tb <- rbind(tb1, tb2)
 tb |>
-  writexl::write_xlsx("Dados/Dados_Brutos/trigo_rendimiento.xlsx")
+  writexl::write_xlsx("Dados/Dados_Brutos/trigo_datas.xlsx")
